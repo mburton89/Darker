806e030d812fdf1919012503e0b66008db0792dd
WEBSTER UNIVERSITY <DTS@WEBSTER.EDU>
2021-02-10 18:52:26 -0600

    scroll shows menu

--- Darker Unity/Assets/_Harris/Code/HUD.cs
+++ Darker Unity/Assets/_Harris/Code/HUD.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject container;
+
+    public List<Image> outlines;
     void Start()
     {
         
@@ -13,6 +16,25 @@ public class HUD : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if(Input.mouseScrollDelta)
+        print("Scroll wheel: " + Input.mouseScrollDelta);
+
+        if (Input.mouseScrollDelta.y == 1 || Input.mouseScrollDelta.y == -1)
+        {
+            ShowMenu();
+        }
+    }
+
+    void ShowMenu()
+    {
+        StopCoroutine(nameof(DelayHide));
+        StartCoroutine(nameof(DelayHide));
+    }
+
+    private IEnumerator DelayHide()
+    {
+        container.SetActive(true);
+        yield return new WaitForSeconds(3);
+        container.SetActive(false);
     }
 }
