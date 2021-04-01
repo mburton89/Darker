7a62dd841e63ea7d9e766b996b69ec91af80f858
WEBSTER UNIVERSITY <DTS@WEBSTER.EDU>
2021-03-31 19:30:24 -0500

    Health interaction started

---
+++ Darker Unity/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public GameObject entity;
+    public int health;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            entity.SetActive(false);
+        }
+    }
+
+    public void Instakill()
+    {
+        health = 0;
+    }
+}
