29874ac91c8f46a070e70a362b0ffc016739fa9b
MElder96 <meghanlelder@gmail.com>
2021-04-21 20:54:26 -0500

    Bare Bones Menu
    Added Picture, Buttons, Text. Start button works. Credits WIP. Exit and Story got nothing.

---
+++ Darker Unity/Assets/_Elder/Main_Menu/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //START THAT GAME BOY
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //EXIT THAT GAME BOY
+    public void ExitGame()
+    {
+        
+    }
+
+    //Read the story
+    public void Story()
+    {
+
+    }
+
+    //Credits Credits
+    public void Credits()
+    {
+
+    }
+
+}
