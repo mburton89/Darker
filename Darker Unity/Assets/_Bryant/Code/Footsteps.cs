44d019ce866f1e62ae569cbc00bf022a8bea06a8
OffDig <bryant.justinm@gmail.com>
2021-04-28 14:36:05 -0500

    Code
    Base Code

---
+++ Darker Unity/Assets/_Bryant/Code/Footsteps.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    private AudioSource footstep_Sound;
+
+    [SerializeField]
+    private AudioClip[] footstep_Clip;
+
+
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
