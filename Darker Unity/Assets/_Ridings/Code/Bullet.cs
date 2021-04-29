241295c05d0c7374a632c8c9a1dec5b8fd6b0622
Zach Ridings <zridings@gmail.com>
2021-04-28 19:12:44 -0500

    a

--- Darker Unity/Assets/_Ridings/Code/Bullet.cs
+++ Darker Unity/Assets/_Ridings/Code/Bullet.cs
@@ -8,11 +8,12 @@ public class Bullet : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
 
-        Destroy(gameObject);
         if ((collision.gameObject.GetComponent("HealthManager") != null))
         {
             collision.gameObject.GetComponent<HealthManager>().health -= 10;
         }
+
+        Destroy(gameObject);
     }
 
 }
